8d7a2ebc9c81c8ca6ccb775804c734f218a8d9da
“OscarToftekaer” <s180875@student.dtu.dk>
2024-04-07 10:06:36 +0200

    scripts added (not done)

---
+++ dataset.py
@@ -0,0 +1,58 @@
+import csv
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torch.nn as nn
+
+class CheXpertDataSet(Dataset):
+    def __init__(self, data_PATH, transform = None, policy = "ones"):
+        """
+        data_PATH: path to the file containing images with corresponding labels.
+        transform: optional transform to be applied on a sample.
+        Upolicy: name the policy with regard to the uncertain labels.
+        """
+        image_names = []
+        labels = []
+
+        with open(data_PATH, "r") as f:
+            csvReader = csv.reader(f)
+            next(csvReader, None) # skip the header
+            for line in csvReader:
+                image_name = line[0]
+                label = line[5:]
+                
+                for i in range(14):
+                    if label[i]:
+                        a = float(label[i])
+                        if a == 1:
+                            label[i] = 1
+                        elif a == -1:
+                            if policy == "ones":
+                                label[i] = 1
+                            elif policy == "zeroes":
+                                label[i] = 0
+                            else:
+                                label[i] = 0
+                        else:
+                            label[i] = 0
+                    else:
+                        label[i] = 0
+                        
+                image_names.append('./' + image_name)
+                labels.append(label)
+
+        self.image_names = image_names
+        self.labels = labels
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """Take the index of item and returns the image and its labels"""
+        image_name = self.image_names[index]
+        image = Image.open(image_name).convert('RGB')
+        label = self.labels[index]
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, torch.FloatTensor(label)
+
+    def __len__(self):
+        return len(self.image_names)
