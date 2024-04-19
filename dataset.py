c0301a1d8a7e5d775db8bf171a1129bd8b4b26c5
Marie Lunde <mao.lunde@gmail.com>
2024-04-19 16:28:58 +0200

    works with full dataset now

--- dataset.py
+++ dataset.py
@@ -17,14 +17,33 @@ def set_seed(seed=1):
 class CheXpertDataset(Dataset):
     def __init__(self, 
                  transform, 
-                 data_dir='./data_frontal/',
+                 data_dir='/dtu/datasets1/ashery-chexpert/data/diffusion_split',
                  num_samples=None,
                  seed=1
     ):
+        # self.data_dir = data_dir
+        # self.npy_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith('.npy')]
+        # self.num_samples = num_samples if num_samples is not None else len(self.npy_files)
+        # self.seed = seed
+
         self.data_dir = data_dir
-        self.npy_files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith('.npy')]
+        self.npy_files = []
+        for folder in os.listdir(data_dir):
+            folder_path = os.path.join(data_dir, folder)
+            for study_folder in os.listdir(folder_path):
+                study_folder_path = os.path.join(folder_path, study_folder)
+                for file in os.listdir(study_folder_path):
+                        file_path = os.path.join(study_folder_path, file)
+                        for file in os.listdir(file_path):
+                            npy_path = os.path.join(file_path, file)
+                            self.npy_files = np.append(self.npy_files,npy_path)
+
+
+        random.seed(seed)
+        random.shuffle(self.npy_files)
         self.num_samples = num_samples if num_samples is not None else len(self.npy_files)
-        self.seed = seed
+        self.npy_files = self.npy_files[:self.num_samples]
+    
 
         # Optionally, reduce dataset size
         if self.num_samples < len(self.npy_files):
