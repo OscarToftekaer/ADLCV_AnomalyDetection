884656655a2fcb9045a3f07dd7d4c12796a02114
MarieLunde <147928420+MarieLunde@users.noreply.github.com>
2024-04-21 17:16:12 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -57,24 +57,32 @@ def save_images(images, path, show=True, title=None, nrow=10):
         plt.show()
     plt.close()
 
-def prepare_dataloader(batch_size, img_size,data_dir,dataset_size):
+def prepare_dataloader(batch_size, img_size,data_dir,dataset_size, RGB = False):
     import torchvision.transforms as transforms
     from torch.utils.data import DataLoader
     from dataset import CheXpertDataset
-    transform = transforms.Compose([
-    transforms.ToTensor(),                # from [0,255] to range [0.0,1.0]
-    transforms.Normalize((0.5,), (0.5,)),   # range [-1,1]
-    transforms.Resize(size=(img_size,img_size))   #resizing to min dimensions
-    ])
-    
+    if RGB == False:
+        transform = transforms.Compose([
+        transforms.ToTensor(),                # from [0,255] to range [0.0,1.0]
+        transforms.Normalize((0.5,), (0.5,)),   # range [-1,1]
+        transforms.Resize(size=(img_size,img_size))   #resizing to min dimensions
+        ])
+    else:
+        transform = transforms.Compose([
+            ToPILImageOrKeep(), 
+            transforms.Grayscale(num_output_channels=3),  # Convert grayscale to RGB
+            transforms.ToTensor(),                  # Convert to tensor
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),  # Normalize
+            transforms.Resize(size=(img_size,img_size))   #resizing to min dimensions
+        ])
     dataset = CheXpertDataset(transform, data_dir = data_dir, num_samples=dataset_size)
     # Split dataset into train and validation sets
-    train_size = int(0.98 * len(dataset))
+    train_size = int(0.995 * len(dataset))
     val_size = len(dataset) - train_size
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
     # Define data loaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=Bbatch_size, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader
 
