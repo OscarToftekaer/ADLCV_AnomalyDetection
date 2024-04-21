0930d3875ef2a944ad905deaca1317d49fa109c7
MarieLunde <147928420+MarieLunde@users.noreply.github.com>
2024-04-21 16:45:41 +0200

    Update val_diffusion.py

--- val_diffusion.py
+++ val_diffusion.py
@@ -7,13 +7,75 @@ from scipy.linalg import sqrtm
 from PIL import Image
 
 from helpers import prepare_dataloader
+from Unet_model import UNet
+from diffmodel import Diffusion
+
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
+
+
+
+diffusion = Diffusion(img_size=128, T=500, beta_start=1e-4, beta_end=0.02, device=device)
+
+
 
 DATASET_SIZE = None
 DATA_DIR = '/dtu/datasets1/ashery-chexpert/data/diffusion_split'
 batch_size = 1
 img_size = 128
 
-_,val_loader = prepare_dataloader(batch_size, img_size,data_dir=DATA_DIR,dataset_size=DATASET_SIZE)
+#load model
+model = UNet(img_size=128, c_in=1, c_out=1, 
+                time_dim=256, channels=32, device=device).to(device)
+model.eval()
+model.to(device)
+model.load_state_dict(torch.load('models/ddpm/weights-30_v2.pt', map_location=device)) # load the given model
+
+
+
+def fid_loop(batch_size, img_size, model, data_dir=DATA_DIR,dataset_size=DATASET_SIZE):
+    t = torch.tensor(200).unsqueeze(0).to(device)
+    _,val_loader = prepare_dataloader(batch_size, img_size,data_dir=DATA_DIR,dataset_size=DATASET_SIZE, RGB = True)
+    inception = inception_v3(pretrained=True, transform_input=False).to(device)
+    inception.eval()
+    features_images = []
+    features_samples = []
+
+    for images in val_loader:
+        with torch.no_grad():
+            images = images.to(device)
+            pred = inception(images.to(device))
+            features_images.append(pred.detach().cpu().numpy())
+
+            x_t, _ = diffusion.q_sample(images, t)   
+            sampled_images = diffusion.ddim_sample_loop(model, x_t, 200, batch_size=images.shape[0])
+            sampled_images = sampled_images.squeeze(0)
+            pred = inception(sampled_images.to(device))
+            features_samples.append(pred.detach().cpu().numpy())
+
+
+    features_images = np.concatenate(features_images, axis=0)
+    features_samples = np.concatenate(features_samples, axis=0)
+
+    # Calculate mean and covariance
+    mu1, sigma1 = features_images.mean(axis=0), np.cov(features_images, rowvar=False)
+    mu2, sigma2 = features_samples.mean(axis=0), np.cov(features_samples, rowvar=False)
+
+    # Calculate FID
+    fid = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
+
+    return fid
+
+
+# Function to calculate Frechet Distance
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    diff = mu1 - mu2
+    covmean = sqrtm(sigma1.dot(sigma2))
+    if np.iscomplexobj(covmean):
+        covmean = covmean.real
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * np.trace(covmean)
+
 
 
 
@@ -29,42 +91,6 @@ def MAD(images, samples):
 
     return MAD_value.item()
 
-
-# FID score
-def calculate_fid(real_images, fake_images, device):
-    # Load pretrained Inception model
-    inception = inception_v3(pretrained=True, transform_input=False).to(device)
-    inception.eval()
-
-    # Calculate features
-    real_features = get_features(real_images, inception, device)
-    fake_features = get_features(fake_images, inception, device)
-
-    # Calculate mean and covariance
-    mu1, sigma1 = real_features.mean(axis=0), np.cov(real_features, rowvar=False)
-    mu2, sigma2 = fake_features.mean(axis=0), np.cov(fake_features, rowvar=False)
-
-    # Calculate FID
-    fid = calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
-    return fid
-
-# Function to calculate Frechet Distance
-
-def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
-    diff = mu1 - mu2
-    covmean = sqrtm(sigma1.dot(sigma2))
-    if np.iscomplexobj(covmean):
-        covmean = covmean.real
-    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * np.trace(covmean)
-
-# Function to get features from inception
-def get_features(images, model, device, batch_size=32):
-    features = []
-    for batch in val_loader:
-        with torch.no_grad():
-            pred = model(batch.to(device))
-            features.append(pred.detach().cpu().numpy())
-    return np.concatenate(features, axis=0)
 
 
 def load_image(image_path):
@@ -83,5 +109,8 @@ def load_image(image_path):
 # test of functions
 images = load_image('results/ddpm/28_v2.jpg')
 samples = load_image('results/ddpm/29_v2.jpg')
-print(l1_loss(images, samples))
-print(MAD(images, samples))
+# print(l1_loss(images, samples))
+# print(MAD(images, samples))
+
+
+print(fid_loop(batch_size, img_size, model, data_dir=DATA_DIR,dataset_size=DATASET_SIZE))
